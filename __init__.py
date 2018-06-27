6b64787fb5cfb796924053f3fed66848ece38e05
Marc Stogaitis <marcsto@gmail.com>
2018-06-27 00:31:12 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,1 @@
-from gym.envs.registration import registry, register, make, spec
-
-register(
-    id='RocketLander-v4',
-    entry_point='gym.envs.box2d:RocketLander',
-    max_episode_steps=1000,
-    reward_threshold=0,
-)
+from rocket_lander import RocketLander
